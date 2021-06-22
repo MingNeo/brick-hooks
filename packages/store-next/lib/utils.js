0b23070bb96adaea2877ba8fd79c7a22d9c0ab53
klose <mingrenjbdtx@gmail.com>
2021-06-22 11:21:15 +0800

    feat: remove package native

--- packages/store-next/lib/utils.js
+++ packages/store-next/lib/utils.js
@@ -1,15 +1,4 @@
 "use strict";
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
 var __read = (this && this.__read) || function (o, n) {
     var m = typeof Symbol === "function" && o[Symbol.iterator];
     if (!m) return o;
@@ -27,22 +16,7 @@ var __read = (this && this.__read) || function (o, n) {
     return ar;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.isObject = exports.combState = exports.combineReducers = exports.combineFlatReducers = void 0;
-function combineFlatReducers(modules, defaultReducers) {
-    if (modules === void 0) { modules = {}; }
-    if (defaultReducers === void 0) { defaultReducers = {}; }
-    return Object.entries(modules).reduce(function (prev, _a) {
-        var _b = __read(_a, 2), moduleName = _b[0], value = _b[1];
-        if (!(value === null || value === void 0 ? void 0 : value.reducers))
-            return prev;
-        Object.entries(value === null || value === void 0 ? void 0 : value.reducers).forEach(function (_a) {
-            var _b = __read(_a, 2), actionName = _b[0], reducer = _b[1];
-            prev[moduleName + "/" + actionName] = reducer;
-        });
-        return prev;
-    }, __assign({}, defaultReducers));
-}
-exports.combineFlatReducers = combineFlatReducers;
+exports.isObject = exports.combState = exports.combineReducers = void 0;
 function combineReducers(reducers, defaultReducers) {
     if (reducers === void 0) { reducers = {}; }
     if (defaultReducers === void 0) { defaultReducers = {}; }
--- packages/store-next/src/index.ts
+++ packages/store-next/src/index.ts
@@ -24,6 +24,7 @@ export {
   createStore,
   registerModule,
   usePlugin,
+  
   // ts types
   Options,
   Module,

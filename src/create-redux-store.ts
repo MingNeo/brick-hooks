11990836b27ceee99ab32cb0e777466e10ad5315
liumingren <mingrenjbdtx@gmail.com>
2021-12-20 14:32:32 +0800

    feat: init

---
+++ src/create-redux-store.ts
@@ -0,0 +1,97 @@
+import { ReducerAction, Reducer } from 'react'
+import { createStore } from 'redux'
+
+export type UnsubscribeFn = () => void
+export type EnhancedStore = any
+
+declare global {
+  // eslint-disable-next-line no-unused-vars
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION__?: any
+  }
+}
+
+interface StoreReducerAction {
+  type: string
+  payload: any
+}
+
+const omit = (obj: Record<string, any>, keyToRemove: string) =>
+  Object.keys(obj)
+    .filter((key) => key !== keyToRemove)
+    .reduce<Record<string, any>>((acc, key) => {
+      acc[key] = obj[key]
+
+      return acc
+    }, {})
+
+function getModuleReducers(reducers: any, moduleName: string) {
+  return Object.entries({
+    ...(reducers?._base || {}),
+    ...(reducers?.[moduleName] || {}),
+  }).map(([reducerName, reducer]) => [moduleName ? `${moduleName}/${reducerName}` : reducerName, reducer])
+}
+
+export const createReduxStore: any = ({ name, initialState = {}, reducers = {} }: any) => {
+  if (typeof window === 'undefined' || !window.__REDUX_DEVTOOLS_EXTENSION__) {
+    return undefined
+  }
+
+  const registeredReducers: Record<string | number, Reducer<any, ReducerAction<any>>> = reducers
+
+  const storeReducer: Reducer<any, StoreReducerAction> = (state, action) => {
+    const actionSplitResult = action.type.split('/')
+    // 当前不支持‘a/b/c'格式action type
+    const moduleName = actionSplitResult.length > 1 ? actionSplitResult[0] : undefined
+    const isInitModule = /\/_initModule$/.test(action.type)
+    const isDeleteAction = /\/_deleteModule$/.test(action.type)
+
+    const currentState = isDeleteAction && moduleName ? omit(state, moduleName) : { ...state }
+
+    const nextState = moduleName
+      ? getModuleReducers(registeredReducers, moduleName).reduce((state, [reducerName, reducer]: [string, any]) => {
+          const moduleState = state[moduleName]
+          if (isInitModule) {
+            state[moduleName] = action.payload
+          } else if (action.type === reducerName) {
+            state[moduleName] = reducer(moduleState, action.payload)
+          }
+          return state
+        }, currentState)
+      : currentState
+
+    return nextState
+  }
+
+  const store: EnhancedStore = createStore(
+    storeReducer,
+    initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__({
+      name: name || 'React state',
+      actionsBlacklist: ['/_deleteModule'],
+    })
+  )
+
+  store.clearModule = (moduleName) => {
+    delete registeredReducers[moduleName]
+
+    store.dispatch({
+      type: `${moduleName}/_deleteModule`,
+    })
+  }
+
+  store.registerModule = (moduleName: string | number, reducers: Reducer<any, unknown>, initialState: any) => {
+    registeredReducers[moduleName] = reducers
+
+    store.dispatch({
+      type: `${moduleName}/_initModule`,
+      payload: initialState,
+    })
+
+    return () => {
+      store.clearModule(moduleName)
+    }
+  }
+
+  return store
+}
---
+++ src/open-redux.ts
@@ -0,0 +1,48 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { createReduxStore, UnsubscribeFn, EnhancedStore } from './create-redux-store'
+
+/**
+ * 当这个插件开启时，store-next的state会同步到redux-dev-tool
+ * 具体来说，开启这个插件的时候，所有的store操作被redux接管，并在redux数据变化后通知到store-next更新数据
+ * @param store store-next的store实例
+ * @param reduxStoreName
+ * @param initialState
+ * @param reducers
+ * @returns
+ */
+export default function openRedux(store, reduxStoreName, initialState, reducers) {
+  // TODO 热更新的情况下需要特殊处理
+  const reduxStore: EnhancedStore = createReduxStore({ name: reduxStoreName, initialState, reducers })
+
+  const dispatch = (action: any) => {
+    if (action && typeof action === 'object' && typeof action.type === 'string') {
+      reduxStore?.dispatch(action)
+    }
+  }
+
+  // 订阅redux store的state并返回解除订阅的函数
+  // 当redux store的state变化时，触发store-next的数据变更
+  const unsubscribe: UnsubscribeFn = reduxStore?.subscribe(() => {
+    const storeState: any = reduxStore?.getState()
+    const prevState = store.getState()
+
+    // 合并实际的module和注册的module
+    Object.keys(storeState).forEach((item) => store._modules.add(item))
+
+    Array.from(store._modules).forEach((modeuleName: string) => {
+      const moduleState = storeState[modeuleName]
+      if (prevState[modeuleName] !== moduleState) {
+        store._setModuleState(modeuleName, moduleState, false)
+      }
+      prevState[modeuleName] = moduleState
+    })
+  })
+
+  return {
+    registerModule: reduxStore?.registerModule,
+    dispatch,
+    unsubscribe,
+    replaceReducer: reduxStore?.replaceReducer,
+    reduxStore,
+  }
+}

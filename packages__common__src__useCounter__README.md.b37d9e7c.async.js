be4fbb6f122358e86042d69820f2015fb22408fe
MingNeo <MingNeo@users.noreply.github.com>
2022-11-04 03:12:25 +0000

    deploy: 4f4613cea67cd1194568303450beba7742c6f093

---
+++ packages__common__src__useCounter__README.md.b37d9e7c.async.js
@@ -0,0 +1,15 @@
+(self.webpackChunk_brick_hooks_root=self.webpackChunk_brick_hooks_root||[]).push([[1512],{73602:function(l,u,n){"use strict";n.d(u,{m:function(){return e.m}});var e=n(67624),t=n(93142)},54952:function(l,u,n){"use strict";n.r(u);var e=n(67294),t=n(36445),s=n(26328),r=n(91485),m=e.memo(o=>{var a=o.demos,c=a["usecounter-demo"].component;return e.createElement(e.Fragment,null,e.createElement(e.Fragment,null,e.createElement("div",{className:"markdown"},e.createElement("h2",{id:"usecounter"},e.createElement(t.AnchorLink,{to:"#usecounter","aria-hidden":"true",tabIndex:-1},e.createElement("span",{className:"icon icon-link"})),"useCounter"),e.createElement("h3",{id:"\u6F14\u793A"},e.createElement(t.AnchorLink,{to:"#\u6F14\u793A","aria-hidden":"true",tabIndex:-1},e.createElement("span",{className:"icon icon-link"})),"\u6F14\u793A")),e.createElement(s.default,a["usecounter-demo"].previewerProps,e.createElement(c,null)),e.createElement("div",{className:"markdown"},e.createElement("h3",{id:"\u7C7B\u578B\u58F0\u660E"},e.createElement(t.AnchorLink,{to:"#\u7C7B\u578B\u58F0\u660E","aria-hidden":"true",tabIndex:-1},e.createElement("span",{className:"icon icon-link"})),"\u7C7B\u578B\u58F0\u660E"),e.createElement(r.Z,{code:`interface CounterOptions {
+  min?: number
+  max?: number
+  step?: number
+}
+function useCounter(initialValue?: number, options?: CounterOptions): [number, Methods]`,lang:"typescript"}),e.createElement("h3",{id:"\u7528\u6CD5"},e.createElement(t.AnchorLink,{to:"#\u7528\u6CD5","aria-hidden":"true",tabIndex:-1},e.createElement("span",{className:"icon icon-link"})),"\u7528\u6CD5"),e.createElement(r.Z,{code:`function MyComponent() {
+  const [count, actions] = useCounter(0, { min: 0, max: 100 })
+  actions.inc() // count: oldCount + 1
+  actions.dec() // count: oldCount - 1
+  actions.inc(3) // count: oldCount + 3
+  actions.dec(3) // count: oldCount - 3
+  actions.reset(3) // count: initialValue
+  actions.set(50) // count: 50
+  return <Child>...</Child>
+}`,lang:"javascript"}))))});u.default=o=>{var a=e.useContext(t.context),c=a.demos;return e.useEffect(()=>{var i;o!=null&&(i=o.location)!==null&&i!==void 0&&i.hash&&t.AnchorLink.scrollToAnchor(decodeURIComponent(o.location.hash.slice(1)))},[]),e.createElement(m,{demos:c})}}}]);
---
+++ packages__web__src__useLazyImage__README.md.e8d3153d.async.js
@@ -0,0 +1,19 @@
+(self.webpackChunk_brick_hooks_root=self.webpackChunk_brick_hooks_root||[]).push([[4840],{73602:function(l,u,n){"use strict";n.d(u,{m:function(){return e.m}});var e=n(67624),t=n(93142)},70997:function(l,u,n){"use strict";n.r(u);var e=n(67294),t=n(36445),m=n(26328),i=n(91485),s=e.memo(a=>{var r=a.demos,o=r["uselazyimage-demo"].component;return e.createElement(e.Fragment,null,e.createElement(e.Fragment,null,e.createElement("div",{className:"markdown"},e.createElement("h2",{id:"uselazyimage"},e.createElement(t.AnchorLink,{to:"#uselazyimage","aria-hidden":"true",tabIndex:-1},e.createElement("span",{className:"icon icon-link"})),"useLazyImage"),e.createElement("p",null,"\u81EA\u52A8\u5BF9\u56FE\u7247\u8FDB\u884C\u61D2\u52A0\u8F7D\u5904\u7406"),e.createElement("h3",{id:"\u6F14\u793A"},e.createElement(t.AnchorLink,{to:"#\u6F14\u793A","aria-hidden":"true",tabIndex:-1},e.createElement("span",{className:"icon icon-link"})),"\u6F14\u793A")),e.createElement(m.default,r["uselazyimage-demo"].previewerProps,e.createElement(o,null)),e.createElement("div",{className:"markdown"},e.createElement("h3",{id:"\u7C7B\u578B\u58F0\u660E"},e.createElement(t.AnchorLink,{to:"#\u7C7B\u578B\u58F0\u660E","aria-hidden":"true",tabIndex:-1},e.createElement("span",{className:"icon icon-link"})),"\u7C7B\u578B\u58F0\u660E"),e.createElement(i.Z,{code:`function useLazyImages(
+  querySelector: string,
+  {
+    sourceAttr,
+    ...ioOptions
+  }?: {
+    sourceAttr?: string
+  },
+): void`,lang:"typescript"}),e.createElement("h3",{id:"\u7528\u6CD5"},e.createElement(t.AnchorLink,{to:"#\u7528\u6CD5","aria-hidden":"true",tabIndex:-1},e.createElement("span",{className:"icon icon-link"})),"\u7528\u6CD5"),e.createElement(i.Z,{code:`function Comp() {
+  useLazyImages('.container img')
+
+  return (
+    <div class="container">
+      <img data-src="http://xxx" />
+      <img data-src="http://xxx" />
+      <img data-rc="http://xxx" />
+    </div>
+  )
+}`,lang:"javascript"}))))});u.default=a=>{var r=e.useContext(t.context),o=r.demos;return e.useEffect(()=>{var c;a!=null&&(c=a.location)!==null&&c!==void 0&&c.hash&&t.AnchorLink.scrollToAnchor(decodeURIComponent(a.location.hash.slice(1)))},[]),e.createElement(s,{demos:o})}}}]);
